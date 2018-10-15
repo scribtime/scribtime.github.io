688b36f3ace96ce1841031179231a75328f980c2
Michael Halwax <mhalwax@gmail.com>
2018-10-15 20:08:31 +0200

    support specifying a date other than today

--- resources/js/main.js
+++ resources/js/main.js
@@ -28,33 +28,41 @@ Vue.component('header-clock', {
   }
 });
 
+function getLang() {
+  return navigator.language || navigator.browserLanguage || (navigator.languages || [ "en" ])[0];
+} 
+
 new Vue({
   el: '#app',
-  data: {
-    time: moment().format('LT'),
-    interval: null,
-    date: moment().format('l'),
-    hours: moment().hours(),
-    minutes: moment().minutes(),
-    start: {
-      input: '',
-      value: '--:--',
-      inputEnabled: true
-    },
-    duration: {
-      input: '',
-      value: '--:--',
-      inputEnabled: true
-    },
-    end: {
-      input: '',
-      value: '--:--',
-      inputEnabled : true
-    },
-    realDurationValue: '--:--',
-    diffDurationValue: '--:--',
-    statusValue: '-',
-    heroText: 'Your greatest resource<br /> is your time.'
+  data: function () {
+    var fullDateFormat = moment.localeData().longDateFormat('L') + ' ' + moment.HTML5_FMT.TIME;
+    return {
+      time: moment().format('LT'),
+      interval: null,
+      date: moment().format('l'),
+      hours: moment().hours(),
+      minutes: moment().minutes(),
+      start: {
+        input: '',
+        value: '--:--',
+        inputEnabled: true
+      },
+      duration: {
+        input: '',
+        value: '--:--',
+        inputEnabled: true
+      },
+      end: {
+        input: '',
+        value: '--:--',
+        inputEnabled: true
+      },
+      realDurationValue: '--:--',
+      diffDurationValue: '--:--',
+      statusValue: '-',
+      heroText: 'Your greatest resource<br /> is your time.',
+      fullDateFormat: fullDateFormat
+    };
   },
   methods: {
     loadTime: function () {
@@ -77,38 +85,38 @@ new Vue({
 
       if (startMoment.isValid() && durationDuration.isValid()) {
 
-        this.start.value = startMoment.format(moment.HTML5_FMT.TIME);
+        this.start.value = this.formatMoment(startMoment);
         this.duration.value = durationDuration.format(moment.HTML5_FMT.TIME, {
           trim: false
         });
-        this.end.value = startMoment.add(durationDuration).format(moment.HTML5_FMT.TIME);
+        this.end.value = this.formatMoment(startMoment.add(durationDuration));
         this.end.inputEnabled = false;
 
       } else if (startMoment.isValid() && endMoment.isValid()) {
 
-        this.start.value = startMoment.format(moment.HTML5_FMT.TIME);
+        this.start.value = this.formatMoment(startMoment);
         this.duration.value = moment.duration(endMoment.diff(startMoment)).format(moment.HTML5_FMT.TIME, {
           trim: false
         });
-        this.end.value = endMoment.format(moment.HTML5_FMT.TIME);
+        this.end.value = this.formatMoment(endMoment);
         this.duration.inputEnabled = false;
 
-      } else if(startMoment.isValid()) {
-        
-        this.start.value = startMoment.format(moment.HTML5_FMT.TIME);
+      } else if (startMoment.isValid()) {
+
+        this.start.value = this.formatMoment(startMoment);
         this.duration.value = '--:--';
         this.end.value = '--:--';
 
         this.end.inputEnabled = true;
         this.duration.inputEnabled = true;
 
-      } else if(durationDuration.isValid() && endMoment.isValid()) {
+      } else if (durationDuration.isValid() && endMoment.isValid()) {
 
-        this.start.value = endMoment.subtract(durationDuration).format(moment.HTML5_FMT.TIME);
+        this.start.value = this.formatMoment(endMoment.subtract(durationDuration));
         this.duration.value = durationDuration.format(moment.HTML5_FMT.TIME, {
           trim: false
         });
-        this.end.value = endMoment.format(moment.HTML5_FMT.TIME);
+        this.end.value = this.formatMoment(endMoment);
         this.start.inputEnabled = false;
 
       } else {
@@ -127,34 +135,45 @@ new Vue({
 
     },
     adjustTime() {
-      
+
       var startMoment = this.toMoment(this.start.input);
       var durationDuration = this.toDuration(this.duration.input);
       var endMoment = this.toMoment(this.end.input);
 
-      if(startMoment.isValid()) {
-        this.start.input = startMoment.format(moment.HTML5_FMT.TIME);
+      
+      if (startMoment.isValid()) {
+        this.start.input = this.formatMoment(startMoment);
       }
-      if(durationDuration.isValid()) {
+      if (durationDuration.isValid()) {
         this.duration.input = durationDuration.format(moment.HTML5_FMT.TIME, {
           trim: false
         });
       }
-      if(endMoment.isValid()) {
-        this.end.input = endMoment.format(moment.HTML5_FMT.TIME);
+      if (endMoment.isValid()) {
+        this.end.input = this.formatMoment(endMoment);
       }
 
     },
-    toMoment(value) {
-      if(_.trim(value) === '') {
+    toMoment(stringValue) {
+      if (_.trim(stringValue) === '') {
         return moment.invalid();
       }
-      return moment(value, moment.HTML5_FMT.TIME);
+      var fullDate = moment(stringValue, this.fullDateFormat, true);
+      if (fullDate.isValid()) {
+        return fullDate;
+      }
+      return moment(stringValue, moment.HTML5_FMT.TIME);
+    },
+    formatMoment(momentValue) {
+      if(momentValue.isSame(moment(), 'day')) {
+        return momentValue.format(moment.HTML5_FMT.TIME);
+      }
+      return momentValue.format(this.fullDateFormat);
     },
     toDuration(value) {
-      if(_.trim(value) === '' || value === '--:--') {
+      if (_.trim(value) === '' || value === '--:--') {
         return moment.duration.invalid();
-      } else if(/^\d+$/.test(value)) {
+      } else if (/^\d+$/.test(value)) {
         return moment.duration(value + ':00');
       } else if (/^\d+?\.\d+$/.test(value)) {
         return moment.duration(parseFloat(value), 'hours');
@@ -172,7 +191,7 @@ new Vue({
       if (startMoment.isValid() && startMoment.isBefore(moment())) {
 
         var tillFixedEndOrNow = moment.duration(moment().diff(startMoment));
-        if(endInputMoment.isValid() && moment().isAfter(endInputMoment)) {
+        if (endInputMoment.isValid() && moment().isAfter(endInputMoment)) {
           tillFixedEndOrNow = moment.duration(endInputMoment.diff(startMoment));
         }
 
@@ -186,7 +205,7 @@ new Vue({
           this.diffDurationValue = diffDuration.format(moment.HTML5_FMT.TIME, {
             trim: false
           });
-          
+
           var tillNowHours = parseFloat(tillFixedEndOrNow.format('h', 2));
           var durationHours = parseFloat(durationDuration.format('h', 2));
 
@@ -195,10 +214,10 @@ new Vue({
 
           this.statusValue = percentage.toFixed(2) + ' % : ' + tillNowHours.toFixed(2) + ' of ' + durationHours.toFixed(2) + '';
 
-          if(endInputMoment.isValid() && moment().isAfter(endInputMoment) && tillNowHours === durationHours) {
+          if (endInputMoment.isValid() && moment().isAfter(endInputMoment) && tillNowHours === durationHours) {
             this.heroText = 'just in time';
-          } else if(tillFixedEndOrNow.asMinutes() <= durationDuration.asMinutes()) {
-            this.heroText = 'time to stop in<br/>' + moment.duration(diffDuration.asMinutes()*-1, 'minutes').format('h [hours], m [minutes]');
+          } else if (tillFixedEndOrNow.asMinutes() <= durationDuration.asMinutes()) {
+            this.heroText = 'time to stop in<br/>' + moment.duration(diffDuration.asMinutes() * -1, 'minutes').format('h [hours], m [minutes]');
           } else {
             this.heroText = 'time exceeded by<br/>' + diffDuration.format('h [hours], m [minutes]');
           }
@@ -213,7 +232,7 @@ new Vue({
         this.realDurationValue = '--:--';
         this.diffDurationValue = '--:--';
 
-        if(startMoment.isValid() && startMoment.isAfter(moment())) {
+        if (startMoment.isValid() && startMoment.isAfter(moment())) {
           this.statusValue = 'starts in ' + moment.duration(startMoment.diff(moment())).format('h [hours], m [minutes]');
           this.heroText = 'Your greatest resource<br/> is your time.';
         } else {
@@ -224,10 +243,10 @@ new Vue({
       }
     },
     visibility(show) {
-      if(show) {
+      if (show) {
         return {};
       }
-      return { display: 'none'};
+      return { display: 'none' };
     }
   },
   mounted: function () {
@@ -235,7 +254,7 @@ new Vue({
     this.interval = setInterval(function () {
       var originalTime = this.time;
       this.loadTime();
-      if(originalTime !== this.time) {
+      if (originalTime !== this.time) {
         this.adjustStats();
       }
     }.bind(this), 1000);
